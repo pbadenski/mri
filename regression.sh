40027f08df12d1929744f465508b0d5d8fdc1d55
Pawel Badenski <pbadensk@thoughtworks.com>
2015-06-30 19:18:37 -0400

    Regression test for sagas

--- regression.sh
+++ regression.sh
@@ -3,3 +3,7 @@ java -jar build/libs/org.mri-1.*.jar --classpath-file "../Axon-trader/this.class
 diff createuser.puml regression/createuser.puml
 java -jar build/libs/org.mri-1.*.jar --classpath-file "../Axon-trader/this.classpath" -s ../Axon-trader/ -m createuser -f dot > createuser.dot
 diff createuser.dot regression/createuser.dot
+java -jar build/libs/org.mri-1.*.jar --classpath-file "../Axon-trader/this.classpath" -s ../Axon-trader/ -m CompanyController.sell -f plantuml > company_controller_sell.puml
+diff company_controller_sell.puml regression/company_controller_sell.puml
+java -jar build/libs/org.mri-1.*.jar --classpath-file "../Axon-trader/this.classpath" -s ../Axon-trader/ -m CompanyController.sell -f dot > company_controller_sell.dot
+diff company_controller_sell.dot regression/company_controller_sell.dot
